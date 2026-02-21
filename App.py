5b4f35a49d903fba9987405046f865974ac16f17
brian123love <mashauribrian2021@gmail.com>
2026-02-21 20:34:06 +0300

    Update App.py
    updated

--- App.py
+++ App.py
@@ -110,18 +110,6 @@ def get_guests():
         for row in rows
     ])
 
-# -----------------------------
-# DELETE GUEST (TEMP FIX)
-# -----------------------------
-@app.route("/delete/<uid>")
-def delete_guest(uid):
-    conn=get_conn()
-    cursor=conn.cursor()
-    cursor.execute("DELETE FROM guests WHERE unique_id=%s"(uid,))
-    conn.commit()
-    conn.close()
-    return"Deleted"
-
 
 # -----------------------------
 # RUN APP
